27ddf204e0e94fce40024dcbb5f7ffdd7033839b
TaniaRivasV <taniarval@gmail.com>
2025-12-05 13:20:29 -0600

    Agrego prueba y función de resta

--- calculadora_tdd/calculator.py
+++ calculadora_tdd/calculator.py
@@ -2,3 +2,6 @@
 class Calculator:
     def add(self, a, b):
         return a + b
+
+    def resta(self, a, b):
+        return a - b
--- calculadora_tdd/tests/test_calculator.py
+++ calculadora_tdd/tests/test_calculator.py
@@ -4,3 +4,8 @@ from calculadora_tdd.calculator import Calculator
 def test_add():
     calc = Calculator()
     assert calc.add(2, 3) == 5
+
+def test_resta():
+    calc = Calculator()
+    assert calc.resta(10, 4) == 6
+    assert calc.resta(3, -2) == 5
